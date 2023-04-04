4223f92f4140211ea59b09fe19916d6b37890c8c
aidanbeale <aidanbeale@gmail.com>
2023-04-04 15:11:10 +1000

    Add components

---
+++ src/components/Food.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Food = ({food}) => {
+  const style = {
+    left: `${food[0]}%`,
+    top: `${food[1]}%`
+  };
+  return <div className="food" style={style} />;
+};
+
+export default Food;
